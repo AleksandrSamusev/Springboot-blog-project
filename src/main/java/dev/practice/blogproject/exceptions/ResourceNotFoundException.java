cf99eddabdfcc187cda86c57ab25177253c98150
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-11-11 04:21:06 +0100

    fix minor ResourceNotFoundException class

--- src/main/java/dev/practice/blogproject/exceptions/ResourceNotFoundException.java
+++ src/main/java/dev/practice/blogproject/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package dev.practice.blogproject.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(Long id, Class<?> entity) {
-        super("The" + entity.getSimpleName().toLowerCase() + " with a given id " + id + " not found");
+    public ResourceNotFoundException(String message) {
+        super(message);
     }
 }
