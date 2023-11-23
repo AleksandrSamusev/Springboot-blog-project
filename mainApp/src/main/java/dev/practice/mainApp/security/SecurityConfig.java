d485c80877bb56170744cde4ac94fd3d5f24a56c
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-11-23 11:29:36 +0100

    add security dependencies and SecurityConfig

---
+++ mainApp/src/main/java/dev/practice/mainApp/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package dev.practice.mainApp.security;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorize) -> {
+            authorize.requestMatchers("/api/v1/private/**").hasAnyRole("ADMIN", "USER");
+            authorize.requestMatchers("/api/v1/admin/**").hasRole("ADMIN");
+            authorize.requestMatchers("/api/v1/public/**").permitAll();
+            authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin-password"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("user-password"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
